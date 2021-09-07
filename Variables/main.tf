e94fbe46c4d0c5ad276bbb13c11d5e25b9233844
genius101 <habibejio@hotmail.com>
2021-09-07 21:57:24 +0100

    the main

---
+++ Variables/main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "vpcname" {
+    type    = string
+    default = "myvpc"
+}
+
+variable "sshport" {
+    type    = number
+    default = 22
+}
+
+variable "enabled" {
+    default = true
+}
+
+variable "mylist" {
+    type    = list(string)
+    default = ["Value0", "Value1", "Value2"] 
+}
+
+variable "mymap" {
+    type    = map
+    default = {
+        key1 = "Value1"
+        key2 = "Value2"
+    }
+}
+#This is the only file to commit
+variable "inputname" {
+    type    = string
+    description = "Set the name of the VPC"
+    default = "inputname"
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block="10.0.0.0/16"
+
+    tags = {
+        Name = var.inputname
+    }
+}
+
+output "vpcid" {
+    value = aws_vpc.myvpc.id
+}
